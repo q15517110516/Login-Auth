fc2c49573bb053c81ab71299ddd38bcc449793da
q15517110516 <lmrzds@gmail.com>
2020-06-04 18:44:22 -0400

    passport

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://lmrzds:LMRlmr960721@login-auth.zdkv5.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://lmrzds:LMRlmr960721@login-auth.zdkv5.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 };
 
