a1838ee050896484854987653a91ad9806cd9dbb
q15517110516 <lmrzds@gmail.com>
2020-06-04 16:17:06 -0400

    build server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// initialize app
+const app = express();
+
+// BodyParser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// MongoDB connect
+mongoose.connect(
+        db,
+        {useNewUrlParser: true}
+    )
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port}`));
