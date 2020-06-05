5d5f51cc146eb52e7ddb8fe57316f9f4cde8452d
q15517110516 <lmrzds@gmail.com>
2020-06-04 20:17:03 -0400

    updates

--- server.js
+++ server.js
@@ -2,26 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
 const users = require("./routes/api/users");
 
 // initialize app
+
 const app = express();
 
-// BodyParser middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
 
 app.use(bodyParser.json());
 
-// DB config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-// MongoDB connect
-mongoose.connect(
+// Connect to MongoDB
+mongoose
+    .connect(
         db,
-        {useNewUrlParser: true}
+        { useNewUrlParser: true }
     )
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
@@ -37,4 +40,4 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server up and running on port ${port}`));
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
