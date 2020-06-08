24dcae9cefd75d34c689af3081881e8837320096
q15517110516 <lmrzds@gmail.com>
2020-06-07 20:54:05 -0400

    first draft landing

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+
+class Landing extends Component {
+    render() {
+        return (
+        <div className="landing">
+            <NavLink to="/register" style={{ width: "140px", borderRadius: "3px", letterSpacing: "1.5px" }}>
+                <button type="button" className="btn btn-primary">Register</button>
+                
+            </NavLink>
+            <NavLink to="/login" style={{ width: "140px", borderRadius: "3px", letterSpacing: "1.5px" }}>
+                <button type="button" className="btn btn-light">Log In</button>
+            </NavLink>
+        </div>
+        );
+    }
+}
+export default Landing;
