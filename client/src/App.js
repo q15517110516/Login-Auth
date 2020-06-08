f3c5615dbad754b3989bb4831504c28a2c83e98f
q15517110516 <lmrzds@gmail.com>
2020-06-07 20:53:41 -0400

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing';
+import { Route } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className="main-page">
         <Navbar/>
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/register" component={Register}/>
+        <Route exact path="/login" component={Login}/>
       </div>
     )
   }
