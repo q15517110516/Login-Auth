aaed9f89664a89433c56f518b6102b9a44f2c2cd
q15517110516 <lmrzds@gmail.com>
2020-06-07 20:53:56 -0400

    navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ import ud from './UDEL.jpg';
 class Navbar extends Component {
     render() {
         return (
-        <div className="navbar">
+        <div>
             <nav className="navbar navbar-light">
                 <NavLink className="navbar-brand" to="/" >
                     <img src={ud} width={100} height={100} alt="ud-logo"/>
