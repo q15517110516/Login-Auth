13d61ce68d2e6a3e932c5329326d55b4ada8fce2
q15517110516 <lmrzds@gmail.com>
2020-06-13 15:45:46 -0400

    create store

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,16 @@
-import { createStore, applymiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-    () => [],
+    rootReducer,
     initialState,
     compose(
-        applymiddleware(...middleware),
+        applyMiddleware(...middleware),
         
     )
 );
