84b9991905caa41b58921bf8efcb919bba88c200
q15517110516 <lmrzds@gmail.com>
2020-06-15 19:58:49 -0400

    edit main page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,13 +6,18 @@ class Landing extends Component {
     render() {
         return (
         <div className="container">
-            <NavLink to="/register" style={{ width: "140px", borderRadius: "3px", letterSpacing: "1.5px" }}>
-                <button type="button" className="btn btn-primary">Register</button>
-                
-            </NavLink>
-            <NavLink to="/login" style={{ width: "140px", borderRadius: "3px", letterSpacing: "1.5px" }}>
-                <button type="button" className="btn btn-light">Log In</button>
-            </NavLink>
+            <h1>Welcome!</h1>
+            <div className="registerBtn" style={{marginBottom: "20px"}}>
+                <NavLink to="/register" style={{ borderRadius: "3px", letterSpacing: "1.5px"}}>
+                    <button type="button" className="btn btn-primary" style={{ width: "140px", height: "50px", fontSize: "20px"}}>Register</button>
+                    
+                </NavLink>
+            </div>
+            <div className="loginBtn">
+                <NavLink to="/login" style={{ borderRadius: "3px", letterSpacing: "1.5px"}}>
+                    <button type="button" className="btn btn-light" style={{ width: "140px", height: "50px", fontSize: "20px"}}>Log In</button>
+                </NavLink>
+            </div>
         </div>
         );
     }
